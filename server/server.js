6c46d4f1289e74f06d6991a1c20cefc8641b87bc
MaximeArn <maxime-arnould@outlook.fr>
2026-02-23 15:27:24 +0100

    test: repare tests

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/api/object", (req, res) => {
   res.status(200).json({
-    message: "Hello, World! It will fail to push 2",
+    message: "Hello, World!",
     timestamp: new Date(),
     status: "success",
   });
